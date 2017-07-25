0a7dbaa57f3eef1730e60a1aef51e822b99bdb74
Roxanne Baldwin <space.legs@Roxannes-MacBook-Air.local>
2017-07-25 16:24:29 -0600

    💯 client-side can be populated by default db values

--- prime.js
+++ prime.js
@@ -1,3 +1,35 @@
 $(document).ready(function() {
-  console.log('working')
+  const baseURL = 'https://intense-bastion-27693.herokuapp.com/'
+
+  $.get(baseURL)
+  .then(appendTable)
 })
+
+function appendTable(data){
+  let $protection = data[0].protection
+  let $shelter = data[0].shelter
+  let $groupNum = data[0].how_many_people_in_group
+  let $transportation = data[0].transportation
+  let $infected = data[0].infected
+
+  let table = `
+        
+            <h3><span class="glyphicon glyphicon-tent" aria-hidden="true"></span></h3>
+              <p>${$shelter}</p>
+              <br>
+            <h3><span class="glyphicon glyphicon-scissors" aria-hidden="true"></span></h3>
+              <p>${$protection}</p>
+              <br>
+            <h3><span class="glyphicon glyphicon-road" aria-hidden="true"></span></h3>
+              <p>${$transportation}</p>
+              <br>
+            <h3><span class="glyphicon glyphicon-user" aria-hidden="true"></span></h3>
+              <p>${$groupNum}</p>
+              <br>
+            <h3><span class="glyphicon glyphicon-tint" aria-hidden="true"></span></h3>
+              <p>${$infected}</p>
+              <br>`
+
+  $('#container').append(table)
+
+}
