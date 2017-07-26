77c4e15fbb49707f9f2d556d26ce12e791768639
Roxanne Baldwin <space.legs@Roxannes-MacBook-Air.local>
2017-07-26 15:32:51 -0600

    client-side is updated with specfic info based on which name is clicked at the original site

--- prime.js
+++ prime.js
@@ -1,58 +1,79 @@
 $(document).ready(function() {
-  const baseURL = 'https://intense-bastion-27693.herokuapp.com/'
+
 
   $.get(baseURL)
   .then(getNames)
 
 })
 
+const baseURL = 'https://intense-bastion-27693.herokuapp.com/meatdecoy'
+
+function addInfo(data){
+  let $protection = data[0].protection
+  let $shelter = data[0].shelter
+  let $groupNum = data[0].how_many_people_in_group
+  let $transportation = data[0].transportation
+  let $infected = data[0].infected
+
+  let table = `
+           <div class="column1">
+            <h3><span class="glyphicon glyphicon-tent" aria-hidden="true"></span></h3>
+              <p>${$shelter}</p>
+              <br>
+            <h3><span class="glyphicon glyphicon-scissors" aria-hidden="true"></span></h3>
+              <p>${$protection}</p>
+              <br>
+            <h3><span class="glyphicon glyphicon-road" aria-hidden="true"></span></h3>
+              <p>${$transportation}</p>
+              <br>
+          </div>
+          <div class="column2">
+            <h3><span class="glyphicon glyphicon-user" aria-hidden="true"></span></h3>
+              <p>${$groupNum}</p>
+              <br>
+            <h3><span class="glyphicon glyphicon-tint" aria-hidden="true"></span></h3>
+              <p>${$infected}</p>
+              <br>
+          </div>`
+
+  $('#tableContainer').html(table)
+
+}
+
+function meatInfo(event, data){
+  event.preventDefault()
+
+  let $clicked = $(event.currentTarget)
+  let $nameVar = $clicked[0].innerText
+
+  for(var i = 0; i < data.length; i++){
+    name = data[i].name
+    if($nameVar === name){
+      const id = data[i].id
+
+      $.get(baseURL + '/' + id)
+      .then(addInfo)
+    }
+  }
+
+  $.get(baseURL + data)
+}
+
 function getNames(data){
-    // console.log(data)
   for(var i = 0; i < data.length; i++){
      name = data[i].name
 
     let nameGroup = `
           <div class="${name}">
-          <button type="button" class="btn btn-default" aria-label="Left Align">
+          <button id="personButton" type="button" class="btn btn-default personButton" aria-label="Left Align">
             <span class="glyphicon glyphicon-unchecked" aria-hidden="true">${name}</span>
-          </button>`
+          </button>
+        </div>`
 
     $('#tableContainer').append(nameGroup)
 
   }
+  $('.personButton').click((event) => {
+    meatInfo(event, data)
+  })
 }
-
-
-
-
-// function appendTable(event){
-//   let $protection = data[0].protection
-//   let $shelter = data[0].shelter
-//   let $groupNum = data[0].how_many_people_in_group
-//   let $transportation = data[0].transportation
-//   let $infected = data[0].infected
-//
-//   let table = `
-//            <div class="column1">
-//             <h3><span class="glyphicon glyphicon-tent" aria-hidden="true"></span></h3>
-//               <p>${$shelter}</p>
-//               <br>
-//             <h3><span class="glyphicon glyphicon-scissors" aria-hidden="true"></span></h3>
-//               <p>${$protection}</p>
-//               <br>
-//             <h3><span class="glyphicon glyphicon-road" aria-hidden="true"></span></h3>
-//               <p>${$transportation}</p>
-//               <br>
-//           </div>
-//           <div class="column2">
-//             <h3><span class="glyphicon glyphicon-user" aria-hidden="true"></span></h3>
-//               <p>${$groupNum}</p>
-//               <br>
-//             <h3><span class="glyphicon glyphicon-tint" aria-hidden="true"></span></h3>
-//               <p>${$infected}</p>
-//               <br>
-//           </div>`
-//
-//   $('#tableContainer').append(table)
-//
-// }
